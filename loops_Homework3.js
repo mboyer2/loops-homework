dccb0afb2266cabf05c094b24594403300a3bb3d
Michael  Boyer <michaelboyer@Michaels-MacBook-Pro.local>
2017-10-17 15:17:28 -0600

    with HW3 complete

--- loops_Homework3.js
+++ loops_Homework3.js
@@ -1,5 +1,8 @@
 
 
+
+// - Meghan is older than 25 Trent is older than 25
+
 var students = [{
  name: 'Liz',
  age: 25,
@@ -22,3 +25,28 @@ var students = [{
  city: 'Denver'
 }];
 
+// - 25 27 32 24 18
+
+for ( var i = 0; i < students.length; i++){
+	console.log(students[i].age)
+}
+
+// - Liz, Boulder Meghan, Denver Trent, Boulder Chelsea, Boulder Amir, Denver
+
+for ( var i = 0; i < students.length; i++){
+	console.log(students[i].name, students[i].city)
+}
+
+// - Liz is from Boulder Trent is from Boulder Chelsea is from Boulder
+
+for ( var i = 0; i < students.length; i++){
+	if (students[i].city === 'Boulder')
+		console.log(students[i].name, students[i].city)
+}
+
+// - Meghan is older than 25 Trent is older than 25
+
+for ( var i = 0; i < students.length; i++){
+	if (students[i].age > 25)
+		console.log(students[i].name)
+}
