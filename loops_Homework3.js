c564a3a17931417839d962fa603c4d2de7fc4485
Michael  Boyer <michaelboyer@Michaels-MacBook-Pro.local>
2017-10-17 13:18:58 -0600

    all files initiated

---
+++ loops_Homework3.js
@@ -0,0 +1,24 @@
+
+
+var students = [{
+ name: 'Liz',
+ age: 25,
+ city: 'Boulder'
+},{
+ name: 'Meghan',
+ age: 27,
+ city: 'Denver'
+},{
+ name: 'Trent',
+ age: 32,
+ city: 'Boulder'
+},{
+ name: 'Chelsea',
+ age: 24,
+ city: 'Boulder'
+},{
+ name: 'Amir',
+ age: 18,
+ city: 'Denver'
+}];
+
