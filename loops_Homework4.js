8939cffd36f16aaa81567700a811c9035252d597
Michael  Boyer <michaelboyer@Michaels-MacBook-Pro.local>
2017-10-23 22:31:22 -0600

    corrected with loops4 question one correct

--- loops_Homework4.js
+++ loops_Homework4.js
@@ -1,70 +1,71 @@
 // For each problem below, write a set of nested loops that prints the given output. Some of the problems will require you to write if statements in the loops.
 // Assume a global array called months is defined as follows:
 
-// var months = [{
-//     name: 'January',
-//     days: 31
-// },{
-//     name: 'February',
-//     days: 28
-// },{
-//     name: 'March',
-//     days: 31
-// },{
-//     name: 'April',
-//     days: 30
-// },{
-//     name: 'May',
-//     days: 31
-// },{
-//     name: 'June',
-//     days: 30
-// },{
-//     name: 'July',
-//     days: 31
-// },{
-//     name: 'August',
-//     days: 31
-// },{
-//     name: 'September',
-//     days: 30
-// },{
-//     name: 'October',
-//     days: 31
-// },{
-//     name: 'November',
-//     days: 30
-// },{
-//     name: 'December',
-//     days: 31
-// }];
+// Problems:
+// ----------
 
+// 1. Write a set of loops that will print out each day of each month in the following format:
+// January 1
+// January 2
+// ...  
+// January 30
+// January 31
+// February 1
+// February 2
+// ...
+// February 27
+// February 28
+// March 1
+// March 2
+// ...
+// Dec 30
+// Dec 31
 
-// // Problems:
-// // ----------
+var months = [{
+    name: 'January',
+    days: 31
+},{
+    name: 'February',
+    days: 28
+},{
+    name: 'March',
+    days: 31
+},{
+    name: 'April',
+    days: 30
+},{
+    name: 'May',
+    days: 31
+},{
+    name: 'June',
+    days: 30
+},{
+    name: 'July',
+    days: 31
+},{
+    name: 'August',
+    days: 31
+},{
+    name: 'September',
+    days: 30
+},{
+    name: 'October',
+    days: 31
+},{
+    name: 'November',
+    days: 30
+},{
+    name: 'December',
+    days: 31
+}];
 
-// // 1. Write a set of loops that will print out each day of each month in the following format:
-// // January 1
-// // January 2
-// // ...  
-// // January 30
-// // January 31
-// // February 1
-// // February 2
-// // ...
-// // February 27
-// // February 28
-// // March 1
-// // March 2
-// // ...
-// // Dec 30
-// // Dec 31
-
-// for ( var i = 0; i < months.length; i++ ){
-//     for ( k = 0; k < months[i].days; k++ ){
-//     console.log(months[i].name, k)
-//     }
-// }
+for ( var i = 0; i < months.length; i++ ){
+    for ( k = 1; k < months[i].days + 1; k++ ){
+    //start at one because there is no day zero add one to months[i].days account for no 0th day
+    
+    console.log(months[i].name, k)
+    }
+}
 
 // 2. Write a set of loops that will print out each second of each hour in a day (in 24 hour clock format).
 
